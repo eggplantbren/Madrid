26b4db5695afaf1e05fc68a67d0f14b216fd3950
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-26 16:48:50 +1300

    Summaries etc.

--- python/grid.py
+++ python/grid.py
@@ -26,6 +26,16 @@ h = prior*lik
 Z = np.trapz(h, x=lambdas)
 post = h/Z
 
+# Calculate and print some summaries
+post_mean = np.trapz(lambdas*post, x=lambdas)
+post_var = np.trapz((lambdas - post_mean)**2 * post, x=lambdas)
+post_sd = np.sqrt(post_var)
+print("lambda = {m} +- {s}".format(m=post_mean, s=post_sd))
+
+# Calculate and print the posterior probability of a statement
+p = np.trapz(post*(lambdas > 5.0), x=lambdas)
+print("P(lambda > 5 | data) = {p}".format(p=p))
+
 # Plot the prior, likelihood, and posterior
 plt.plot(lambdas, prior, label="Prior")
 plt.plot(lambdas, lik, label="Likelihood", alpha=0.2, linestyle="--")
@@ -33,7 +43,5 @@ plt.plot(lambdas, post, label="Posterior")
 plt.xlabel("$\\lambda$")
 plt.ylabel("Probability Density")
 plt.legend()
-plt.show()
-
 plt.show()
 
